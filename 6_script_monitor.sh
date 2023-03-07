36beb71a09cf5a3a0505535243325bf07c76b9f2
mjsidevops <mahammadjabirsi@gmail.com>
2023-03-07 01:47:43 +0000

    monitoring scripts

---
+++ 6_script_monitor.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ls /var/run/httpd/httpd.pid
+if [ $? -eq 0 ]
+then 
+   echo "Httpd is running"
+else
+   echo "Httpd is Not running"
+   echo "Start the httpd process"
+   systemctl start httpd
+   if [ $? -eq 0 ]
+   then
+     echo "Process is running"
+   else
+     echo "Process start failed"
+   fi
+fi
